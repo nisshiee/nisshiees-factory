da85c95e2b5fa1e3964a28c888f9afa3e16ad3bd
Hirokazu Nishioka <hiro@nisshiee.org>
2020-05-14 12:23:00 +0900

    paramsとContextまわりをcommonに移動

--- switchbot-thermometer-holder/src/hook.ts
+++ switchbot-thermometer-holder/src/hook.ts
@@ -1,5 +1,5 @@
 import hook from "./switchbot-thermometer-holder/hook";
-import { Context, Quality } from "common";
+import { parameterDefinitions, paramsToContext } from "common";
 
 // NOTE: 一応この出力で使えたけど、もう1回作るなら改善の余地あり
 //       - 厚さ1.6mmだと少し心もとない
@@ -7,29 +7,11 @@ import { Context, Quality } from "common";
 //       - 爪の厚みは1mmじゃちょっとゆるい
 //       - 温湿度計ケースの下を支える形にしたい
 
-const paramToContext = (params) => {
-  let quality = Quality.Draft;
-  switch (params.quality) {
-    case "smooth":
-      quality = Quality.Smooth;
-  }
-  return new Context(quality);
-};
-
 export const main = (params) => {
-  const ctx = paramToContext(params);
+  const ctx = paramsToContext(params);
   return hook(ctx);
 };
 
 export const getParameterDefinitions = () => {
-  return [
-    {
-      name: "quality",
-      type: "choice",
-      caption: "Quality:",
-      values: ["draft", "smooth"],
-      captions: ["Draft (no rounded corners)", "Smooth (rounded corners)"],
-      initial: "draft",
-    },
-  ];
+  return parameterDefinitions;
 };
